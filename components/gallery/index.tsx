31de4c01f5cc38483a2f8b8b2a6f2ac38fc5fd4a
Kacper Pirog <kacper.pirog@gmail.com>
2023-12-07 20:57:16 +0100

    add component gallery

---
+++ components/gallery/index.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./nav.module.css";
+
+const Gallery = () => {
+  return <div>Gallery</div>;
+};
+
+export default Gallery;
