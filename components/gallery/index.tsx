7c45cc261c188e52b34bd3f718d92bd66f033c70
Kacper Pirog <kacper.pirog@gmail.com>
2023-12-10 12:35:11 +0100

    add to gallery localdata  Gallery

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -8,8 +8,36 @@ interface Gallery {
   image: string;
 }
 
+const gallery: Gallery[] = [
+  {
+    id: 1,
+    title: "gallery_01",
+    image: require("../../ass/gallery/gallery_01.png"),
+  },
+  {
+    id: 2,
+    title: "gallery_02  ",
+    image: require("../../ass/gallery/gallery_02.png"),
+  },
+  {
+    id: 3,
+    title: "gallery_03 ",
+    image: require("../../ass/gallery/gallery_03.png"),
+  },
+  {
+    id: 4,
+    title: "gallery_04",
+    image: require("../../ass/gallery/gallery_04.png"),
+  },
+  {
+    id: 5,
+    title: "gallery_05",
+    image: require("../../ass/gallery/gallery_05.png"),
+  },
+];
+
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return <div>lorem</div>;
 };
 
 export default Gallery;
