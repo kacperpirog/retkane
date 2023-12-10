331afa3718a7c89968060162577da4d42199b47e
Kacper Pirog <kacper.pirog@gmail.com>
2023-12-10 11:58:58 +0100

    add photo to assets

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./nav.module.css";
+import styles from "./gallery.module.css";
+
+type Gallery = {
+  id: number;
+  title: string;
+  image: string;
+};
 
 const Gallery = () => {
   return <div>Gallery</div>;
