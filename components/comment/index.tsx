da47a8076ab3bf49753855dd9849cb532efd6bf9
Kacper Pirog <kacper.pirog@gmail.com>
2023-12-12 21:10:42 +0100

    add to stars to comment

--- components/comment/index.tsx
+++ components/comment/index.tsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import firebase from "firebase/app";
 import "firebase/firestore";
+import Image from "next/image";
+import styles from "./comment.module.css";
+
 type Comment = {
   text: string;
   date: Date;
@@ -8,35 +11,12 @@ type Comment = {
   image: string;
 };
 
-const defaultImage = "/ass/comment/foto.png";
+const defaultImage = "../../ass/comment/foto.png";
 const CommentWrapper = () => {
   const [comments, setComments] = useState<Comment[]>([]);
   const [newComment, setNewComment] = useState<string>("");
   const [newRating, setNewRating] = useState<number>(0);
   const [newImage, setNewImage] = useState<File | null>(null);
-
-  // useEffect(() => {
-  //   const db = firebase.firestore();
-  //   db.collection("comments")
-  //     .orderBy("date", "desc")
-  //     .onSnapshot((snapshot) => {
-  //       const newComments: Comment[] = snapshot.docs.map(
-  //         (doc) => doc.data() as Comment
-  //       );
-  //       setComments(newComments);
-  //     });
-  // }, []);
-
-  // const handleAddComment = () => {
-  //   const db = firebase.firestore();
-  //   db.collection("comments").add({
-  //     text: newComment,
-  //     date: firebase.firestore.Timestamp.fromDate(new Date()),
-  //     rating: newRating,
-  //   });
-  //   setNewComment("");
-  //   setNewRating(0);
-  // };  const [comments, setComments] = useState<Comment[]>([]);
 
   const handleAddComment = () => {
     const reader = new FileReader();
@@ -60,46 +40,69 @@ const CommentWrapper = () => {
   };
 
   return (
-    <div>
-      <h2>Komentarze</h2>
+    <div className={styles.commentSection}>
+      <div>
+        <h2>What do they say about us</h2>
+      </div>
       <input
+        className={styles.inputField}
         type="text"
         value={newComment}
         onChange={(e) => setNewComment(e.target.value)}
       />
-      <select
-        value={newRating}
-        onChange={(e) => setNewRating(Number(e.target.value))}
-      >
-        {[...Array(5)].map((_, i) => (
-          <option key={i} value={i + 1}>
-            {i + 1} {i + 1 > 1 ? "gwiazdki" : "gwiazdka"}
-          </option>
-        ))}
-      </select>
+      <div>
+        <select
+          className={styles.selectRating}
+          value={newRating}
+          onChange={(e) => setNewRating(Number(e.target.value))}
+        >
+          {[...Array(5)].map((_, i) => (
+            <option key={i} value={i + 1}>
+              {i + 1} {i + 1 > 1 ? "gwiazdki" : "gwiazdka"}
+            </option>
+          ))}
+        </select>
+      </div>
       <input
+        className={styles.uploadButton}
         type="file"
         accept="image/*"
         onChange={(e) => setNewImage(e.target.files ? e.target.files[0] : null)}
       />
-      <button onClick={handleAddComment}>Dodaj komentarz</button>
-      {comments.slice(0, 3).map((comment, index) => (
-        <div key={index}>
-          <p>{comment.text}</p>
-          <p>{comment.date.toLocaleString()}</p>
-          <p>Ocena: {"⭐".repeat(comment.rating)}</p>
-          <img src={comment.image} alt="Komentarz użytkownika" />
-        </div>
-      ))}
+      <button className={styles.commentButton} onClick={handleAddComment}>
+        Dodaj komentarz
+      </button>
+      <div className={styles.comments}>
+        {comments.slice(0, 3).map((comment, index) => (
+          <div key={index} className={styles.comment}>
+            <p>{comment.text}</p>
+            <p>{comment.date.toLocaleString()}</p>
+            <p className={styles.commentRatingDisplay}>
+              Ocena: {"⭐".repeat(comment.rating)}
+            </p>
+            <img
+              className={styles.commentImage}
+              src={comment.image}
+              alt="użytkownik"
+            />
+          </div>
+        ))}
+      </div>
       {comments.length > 3 && (
-        <details>
+        <details className={styles.details}>
           <summary>Rozwiń</summary>
           {comments.slice(3).map((comment, index) => (
-            <div key={index + 3}>
+            <div key={index + 3} className={styles.comment}>
               <p>{comment.text}</p>
               <p>{comment.date.toLocaleString()}</p>
-              <p>Ocena: {"⭐".repeat(comment.rating)}</p>
-              <img src={comment.image} alt="Komentarz użytkownika" />
+              <p className={styles.commentRatingDisplay}>
+                Ocena: {"⭐".repeat(comment.rating)}
+              </p>
+              <img
+                className={styles.commentImage}
+                src={comment.image}
+                alt="użytkownik"
+              />
             </div>
           ))}
         </details>
@@ -107,4 +110,5 @@ const CommentWrapper = () => {
     </div>
   );
 };
+
 export default CommentWrapper;
