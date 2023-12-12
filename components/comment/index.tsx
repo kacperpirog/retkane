4aaee3adbcd9c1662522548e8f578a0c3beea5a5
Kacper Pirog <kacper.pirog@gmail.com>
2023-12-12 14:17:19 +0100

    add state and function handleAddComment to comment

--- components/comment/index.tsx
+++ components/comment/index.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
-
+type Comment = {
+  text: string;
+  date: Date;
+};
 const comment = () => {
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [newComment, setNewComment] = useState<string>("");
   return <div>comment</div>;
 };
 
