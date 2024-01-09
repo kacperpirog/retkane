8f1125182e4aeefca62728e29cd7963fe00a1ce7
Kacper Pirog <kacper.pirog@gmail.com>
2024-01-09 22:31:06 +0100

    add copy and image to footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,27 +1,75 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./footer.module.css";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
     <>
       <div className={styles.containerFooter}>
-        <div>
-          <Image
-            className={styles.imageInfo}
-            src={require("../../ass/info/info_foto.jpg")}
-            alt="info"
-          />
-          <Image
-            className={styles.imageInfo}
-            src={require("../../ass/info/info_foto.jpg")}
-            alt="info"
-          />
-          <Image
-            className={styles.imageInfo}
-            src={require("../../ass/info/info_foto.jpg")}
-            alt="info"
-          />
+        <div className={styles.divFooter}>
+          <div className={styles.wrapperFooter}>
+            <h1>Lorem ipsum dolor</h1>
+          </div>
+          <div className={styles.wrapperFooterImageInfo}>
+            <Image
+              className={styles.imageInfo}
+              src={require("../../ass/footer/fb.png")}
+              alt="fb"
+            />
+            <Image
+              className={styles.imageInfo}
+              src={require("../../ass/footer/wa.png")}
+              alt="wa"
+            />
+            <Image
+              className={styles.imageInfo}
+              src={require("../../ass/footer/ig.png")}
+              alt="ig"
+            />
+          </div>
+          <div className={styles.linkFooter}>
+            <Link
+              to="Home"
+              className={styles.linkFooter}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              Home
+            </Link>
+            <Link
+              to="O Mnie"
+              className={styles.linkFooter}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              O Mnie
+            </Link>
+            <Link
+              to="Warsztaty"
+              className={styles.linkFooter}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              Warsztaty
+            </Link>
+            <Link
+              to="Kontakt"
+              className={styles.linkFooter}
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              duration={500}
+            >
+              Kontakt
+            </Link>
+          </div>
         </div>
       </div>
       <div className={styles.footer}>
