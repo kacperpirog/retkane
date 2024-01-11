57844d01b02b07cc20a73ca26252f217b9d4fba6
Kacper Pirog <kacper.pirog@gmail.com>
2024-01-11 15:32:32 +0100

    fix problem add to head meta content widthdevicewidth

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,7 +36,6 @@ export default function Layout({
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
